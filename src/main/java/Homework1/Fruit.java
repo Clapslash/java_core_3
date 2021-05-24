f9a45ba3b65b3089b3ba5cc872f4c3172f75f0bd
Clapslash <bulat.fatkullin.90@mail.ru>
2021-05-24 12:56:54 +0300

    Homework 1

---
+++ src/main/java/Homework1/Fruit.java
@@ -0,0 +1,17 @@
+package Homework1;
+
+public class Fruit {
+    float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
